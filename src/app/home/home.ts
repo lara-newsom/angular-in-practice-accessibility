38077c69c7facaff04366d31dd3bb06becd40842
Lara Newsom <laramnewsom@gmail.com>
2026-02-23 22:30:34 -0600

    accessibility updates

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -12,19 +12,16 @@ export class Home {
   galleryImages = signal([
     {
       src: 'assets/medium/lobby_1.jpg',
-      alt: 'Learn more about The Every Hotel',
       route: '/about',
       cta: 'About Us'
     },
     {
       src: 'assets/medium/room_1.jpg',
-      alt: 'Learn more about our rooms and suites',
       route: '/rooms',
       cta: 'View our Rooms'
     },
     {
       src: 'assets/medium/amenities_1.jpg',
-      alt: 'Learn more about our amenities and services',
       route: '/amenities',
       cta: 'Explore our Amenities'
     }
