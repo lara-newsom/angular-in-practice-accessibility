657f058b1ebbd947fd35c422497ad6eeb1a51d64
Lara Newsom <laramnewsom@gmail.com>
2026-03-09 18:53:25 -0500

    starting point

---
+++ src/app/shared/main-content/header-content/header-content.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { StyledPulseButton } from '../../directives/styled-pulse-button';
+
+@Component({
+  selector: 'app-header-content',
+  imports: [RouterLink, StyledPulseButton],
+  templateUrl: './header-content.html',
+  styleUrl: './header-content.scss',
+})
+export class HeaderContent {
+  /**
+   * The displayed h1 title element for this section
+   */
+  mainHeader = input.required<string>();
+}
