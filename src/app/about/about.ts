6d6f968c1ff695f6506735aac018c2f575150715
Lara Newsom <laramnewsom@gmail.com>
2026-03-15 20:16:32 -0500

    added about

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderContent } from '../shared/main-content/header-content/header-content';
-import { SubHeaderContent } from "../shared/main-content/sub-header-content/sub-header-content";
+import { SubHeaderContent } from '../shared/main-content/sub-header-content/sub-header-content';
 
 @Component({
   selector: 'app-about',
-  imports: [HeaderContent, SubHeaderContent],
+  imports: [
+    HeaderContent,
+    SubHeaderContent,
+  ],
   templateUrl: './about.html',
   styleUrls: ['./about.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
