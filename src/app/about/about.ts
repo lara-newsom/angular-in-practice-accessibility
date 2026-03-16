a9aa59752b5f0bc1cd251368ce5220ba271ad83b
Lara Newsom <laramnewsom@gmail.com>
2026-03-15 21:47:23 -0500

    added accordion

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderContent } from '../shared/main-content/header-content/header-content';
 import { SubHeaderContent } from '../shared/main-content/sub-header-content/sub-header-content';
+import {
+  AccordionGroup,
+  AccordionTrigger,
+  AccordionPanel,
+  AccordionContent,
+} from '@angular/aria/accordion';
 
 @Component({
   selector: 'app-about',
   imports: [
     HeaderContent,
     SubHeaderContent,
+    AccordionGroup,
+    AccordionTrigger,
+    AccordionPanel,
+    AccordionContent,
   ],
   templateUrl: './about.html',
   styleUrls: ['./about.scss'],
